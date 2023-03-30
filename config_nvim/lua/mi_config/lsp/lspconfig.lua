0296d7f39cb4b5053203e08b39fb638ec92ee51b
hayt <camador.git@gmail.com>
2023-03-30 18:14:33 +0200

    004-keymaps: Añadidas descripciones para keymaps de LSP

--- config_nvim/lua/mi_config/lsp/lspconfig.lua
+++ config_nvim/lua/mi_config/lsp/lspconfig.lua
@@ -16,10 +16,10 @@ lspconfig.solargraph.setup {}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<leader>l', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>l', vim.diagnostic.open_float, { desc = '[L]ist current line diagnostics' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Goto previous diagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Goto next diagnostic' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Show all diagnostics on location list' })
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -32,23 +32,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts, { desc = '[G]oto symbol [D]eclaration' })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts, { desc = '[G]oto symbol [d]efinition' })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts, { desc = 'Show info for current symbol' })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts, { desc = 'Show all [i]mplementations of symbol on Quickfix' })
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts, { desc = 'Show signature help' })
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts, { desc = '[W]orkspace: [a]dd folder' })
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts, { desc = '[W]orkspace: [r]emove folder' })
     vim.keymap.set('n', '<leader>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    end, opts, { desc = '[W]orkspace: [l]ist folders' })
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts, { desc = 'Goto to type [D]efinition' })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts, { desc = '[R]e[n]ame symbol' })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts, { desc = 'Call [c]ode [a]ctions' })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts, { desc = 'Show all [r]eferences of symbol on Quickfix' })
     vim.keymap.set('n', '<leader>f', function()
       vim.lsp.buf.format { async = true }
-    end, opts)
+    end, opts, { desc = '[F]ormat current buffer code' })
   end,
 })
 
