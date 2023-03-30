9aae0e4bb9a45d507adbcc9bce8175d1a0e98ae9
hayt <camador.git@gmail.com>
2023-03-30 17:34:02 +0200

    004-keymaps: Añadidas descripciones para algunos keymaps

--- config_nvim/lua/mi_config/keymaps.lua
+++ config_nvim/lua/mi_config/keymaps.lua
@@ -3,7 +3,12 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 -- Shorten function name
-local keymap = vim.api.nvim_set_keymap
+local keymap = function(mode, shortcut, action, options, desc)
+  if desc then
+    options["desc"] = desc
+  end
+  vim.api.nvim_set_keymap(mode, shortcut, action, options)
+end
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
@@ -20,22 +25,22 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts, "Cambiar a la ventana izquierda")
+keymap("n", "<C-j>", "<C-w>j", opts, "Cambiar a la ventana inferior")
+keymap("n", "<C-k>", "<C-w>k", opts, "Cambiar a la ventana superior")
+keymap("n", "<C-l>", "<C-w>l", opts, "Cambiar a la ventana derecha")
 
 --keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>", opts, "Aumentar el tamaño horizontal de la ventana")
+keymap("n", "<C-Down>", ":resize -2<CR>", opts, "Reducir el tamaño horizontal de la ventana")
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts, "Aumentar el tamaño vertical de la ventana")
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts, "Aumentar el tamaño vertical de la ventana")
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts, "Ir al buffer siguiente")
+keymap("n", "<S-h>", ":bprevious<CR>", opts, "Ir al buffer anterior")
 
 -- Insert --
 -- Press jk fast to enter
@@ -43,28 +48,28 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", opts, "Aumentar la indentación y conserva la selección")
+keymap("v", ">", ">gv", opts, "Reducir la indentación y conserva la selección")
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts, "Mover el texto seleccionado hacia abajo")
+keymap("v", "<A-k>", ":m .-2<CR>==", opts, "Mover el texto seleccionado hacia arriba")
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts, "Mover el texto seleccionado hacia abajo")
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts, "Mover el texto seleccionado hacia arriba")
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts, "Mover el texto seleccionado hacia abajo")
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts, "Mover el texto seleccionado hacia arriba")
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts, "Cambiar al terminal izquierdo")
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts, "Cambiar al terminal inferior")
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts, "Cambiar al terminal superior")
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts, "Cambiar al terminal derecho")
 
 -- nvim-tree
-keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
-keymap("n", "<leader>E", ":NvimTreeFindFile<cr>", opts)
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts, "Abrir/cierra el [e]xplorador de ficheros")
+keymap("n", "<leader>E", ":NvimTreeFindFile<cr>", opts,"Abrir el [E]xplorador de ficheros y seleccionar el fichero del buffer" )
