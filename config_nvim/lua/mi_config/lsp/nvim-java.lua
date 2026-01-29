ffd43c0d843e0dbad5cb7c969041250196c5728a
camador <camador.git@gmail.com>
2026-01-29 08:46:46 +0100

    008-LSP: Actualizada configuración de nvim-java

--- config_nvim/lua/mi_config/lsp/nvim-java.lua
+++ config_nvim/lua/mi_config/lsp/nvim-java.lua
@@ -1,2 +1,1 @@
-require('java').setup()
-
+-- require('java').setup({})
--- config_nvim/lua/mi_config/plugins.lua
+++ config_nvim/lua/mi_config/plugins.lua
@@ -78,37 +78,24 @@ require('lazy').setup({
   -- Java - https://github.com/nvim-java/nvim-java
   {
     'nvim-java/nvim-java',
-    dependencies = {
-      'nvim-java/lua-async-await',
-      'nvim-java/nvim-java-refactor',
-      'nvim-java/nvim-java-core',
-      'nvim-java/nvim-java-test',
-      'nvim-java/nvim-java-dap',
-      'MunifTanjim/nui.nvim',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-dap',
-      {
-        'williamboman/mason.nvim',
-        opts = {
-          registries = {
-            'github:nvim-java/mason-registry',
-            'github:mason-org/mason-registry',
-          },
-        build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-        },
-      }
-    },
+    config = function()
+      require('java').setup()
+    end,
   },
 
-  -- Ya incluido con nvim-java. Descomentar si se elimina ese plugin
-  -- {
-  --   "williamboman/mason.nvim",    -- simple to use language server installer
-  --   build = ":MasonUpdate",       -- :MasonUpdate updates registry contents
-  -- },
+  {
+    'williamboman/mason.nvim',    -- simple to use language server installer
+    opts = {
+      registries = {
+        'github:nvim-java/mason-registry',
+        'github:mason-org/mason-registry',
+      },
+      build = ":MasonUpdate",       -- :MasonUpdate updates registry contents
+    },
+  },
   "williamboman/mason-lspconfig.nvim", -- bridge between lspconfig and mason
 
-  -- Ya incluido con nvim-java. Descomentar si se elimina ese plugin
-  --"neovim/nvim-lspconfig",             -- enable LSP
+  "neovim/nvim-lspconfig",             -- enable LSP
 
   --"jose-elias-alvarez/null-ls.nvim",  -- Formatting, linting, diagnostics, code actions (ARCHIVADO)
   "nvimtools/none-ls.nvim",  -- Formatting, linting, diagnostics, code actions
